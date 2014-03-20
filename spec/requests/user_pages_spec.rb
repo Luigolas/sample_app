95cbe338efd146951edc1d0ab0c8701c2b4428f0
Luis González <luigolas@gmail.com>
2014-03-20 20:54:02 +0000

    Implement user authentication

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-
 describe "UserPages" do
   subject {page}
 
@@ -27,6 +26,18 @@ describe "UserPages" do
       it "should not create a user" do
         expect {click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content("Name can't be blank") }
+        it { should have_content("Email can't be blank") }
+        it { should have_content("Email is invalid") }
+        it { should have_content("Password can't be blank") }
+        it { should have_content("Password is too short (minimum is 6 characters)") }
+      end
     end
 
     describe "with valid information" do
@@ -38,6 +49,15 @@ describe "UserPages" do
       end
       it "should create a user" do
         expect {click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
