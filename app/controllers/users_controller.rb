1ee7018afb564c58cbf8891cbce8a897a85470c5
Luis González <luigolas@gmail.com>
2014-03-26 19:42:06 +0000

    Refactor and add more tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user, only: [:destroy]
+  before_action :signed_out_user, only: [:new, :create]
+  before_action :signed_in_user,  only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: [:destroy]
 
   def show
     @user = User.find(params[:id])
@@ -39,9 +40,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url
+    user = User.find(params[:id])
+    if current_user != user
+      user.destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    else
+      redirect_to(root_url)
+    end
   end
 
   private
@@ -51,7 +57,9 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # Before filters
+    def signed_out_user
+      redirect_to(root_url) if signed_in?
+    end
 
     def signed_in_user
       unless signed_in?
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,11 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
+      it { should_not have_link('Users',       href: users_path) }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out',    href: signout_path) }
+      it { should have_link('Sign in',         href: signin_path) }
 
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -46,6 +51,24 @@ describe "Authentication" do
 
   describe "authorization" do
 
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#new action" do
+        before { get new_user_path }
+
+        specify { expect(response).to redirect_to(root_url) }
+        #it { should_not have_title('Sign Up') }
+      end
+
+      describe "when attempting to create user" do
+        before { post users_path, user: FactoryGirl.attributes_for(:user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+    end
+
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -58,8 +81,24 @@ describe "Authentication" do
         end
 
         describe "after sign_in" do
+
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the profile page" do
+              expect(page).to have_title(user.name)
+            end
+
           end
         end
 
@@ -112,5 +151,16 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }
       end
     end
+
+    describe "as admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before { sign_in admin, no_capybara: true }
+
+      describe "submitting a DELETE request to delete itself" do
+        before { delete user_path(admin) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
   end
 end
